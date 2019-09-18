415639d98b25a4657ccda24d8d5aed7abf62591e
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-09-18 10:01:36 +0800

    初始化项目-整理目录-准备资源
    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入ElementUI组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入ElementUI样式
+import './style/index.less' // 引入index样式
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局使用ElementUI
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/home/index' 下列代码等同
+import Home from './views/home/'
 
 Vue.use(Router)
 
@@ -8,16 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
