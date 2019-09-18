aa109ad8038baf9c6dd142b0bb0ea613fd16726e
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-09-18 20:56:22 +0800

    主页模块-页面布局
    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>我是主页</div>
+  <!-- 外层容器 -->
+  <el-container>
+    <!-- 侧边栏 -->
+    <el-aside>左侧</el-aside>
+    <!-- 右侧容器 -->
+    <el-container>
+      <!-- 顶栏 -->
+      <el-header>顶部</el-header>
+      <!-- 主要区域 -->
+      <el-main>
+        <!-- 二级路由 -->
+        二级路由
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
