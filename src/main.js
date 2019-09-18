5adc0004f5121772a3dcc0635626f817d6e0f3d1
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-09-18 19:53:28 +0800

    安装axios-配置全局使用
    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入ElementUI组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入ElementUI样式
 import './style/index.less' // 引入index样式
+import axios from 'axios' // 引入axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 设置axios的默认URL
+Vue.prototype.$axios = axios // 将axios共享给所有Vue或者组件实例使用
 
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局使用ElementUI
