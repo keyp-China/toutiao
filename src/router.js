c04bc38ce3f2b4eebadbfd7220cd12ae3eb7ecbc
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-09-20 22:34:26 +0800

    响应数据拦截与执行失败处理
    响应数据拦截与执行失败处理

--- src/router.js
+++ src/router.js
@@ -4,6 +4,12 @@ import Home from './views/home/'
 import Main from './views/home/main'
 import Login from './views/login'
 
+// 在使用ElementUi时点击同一个路由，页面报错解决方法
+const originalPush = Router.prototype.push
+Router.prototype.push = function (location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 Vue.use(Router)
 
 export default new Router({
--- src/utils/axios-config.js
+++ src/utils/axios-config.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
+import router from '../router'
+import { Message } from 'element-ui'
 
-// axios请求拦截器
+// axios请求数据拦截器
 axios.interceptors.request.use(function (config) {
   // 获取token
   let token = window.localStorage.getItem('user_token')
@@ -10,6 +12,41 @@ axios.interceptors.request.use(function (config) {
 }, function (error) {
   // 对请求失败做处理
   return Promise.reject(error)
+})
+
+// axios响应数据拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做处理
+  return response.data
+}, function (error) {
+  // 执行失败时执行
+  let status = error.response.status // 获取失败的状态码
+  let message = '未知错误'
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 403:
+      message = '403 refresh_token未携带或已过期'
+      break
+    case 507:
+      message = '服务器数据库异常'
+      break
+    case 401:
+      message = 'token过期或未出'
+      window.localStorage.clear() // 清空缓存
+      router.push('/login') // this.$router.push()
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    default:
+      break
+  }
+  // 提示框
+  Message({ message, type: 'error' })
+  // 希望 在异常处理函数中将所有的错误都处理完毕 不再进入catch  终止错误
+  return new Promise(function () { }) // 终止当前的错误
 })
 
 export default axios
