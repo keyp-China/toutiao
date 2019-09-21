639c5cf076157ecf96f2d7ed0da3060a3c090051
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-09-21 19:27:01 +0800

    面包屑组件封装
    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 
 // 全局注册所有普通组件
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
