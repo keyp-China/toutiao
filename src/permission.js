ae412fcff81408f5ea85030b943b1d3166033582
keyp-China <53910114+keyp-China@users.noreply.github.com>
2019-09-26 20:57:19 +0800

    nprogress进度条模块使用
    nprogress进度条模块使用

--- src/permission.js
+++ src/permission.js
@@ -1,7 +1,11 @@
 import router from './router'
+import nprogress from 'nprogress' // 引入nprogress模块
+import 'nprogress/nprogress.css' // 引入nprogress的css
 
 // 全局前置守卫
 router.beforeEach((to, from, next) => {
+  // 进度条开始
+  nprogress.start()
   if (to.path === '/login') {
     // 如果请求的是login 则放行
     next()
@@ -9,6 +13,12 @@ router.beforeEach((to, from, next) => {
     // 判断是否具有token 有则放行 没有则跳转login
     window.localStorage.getItem('user_token') ? next() : next('/login')
   }
+})
+
+// 后置守卫
+router.afterEach(() => {
+  // 进度条结束
+  nprogress.done()
 })
 
 export default router
